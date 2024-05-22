dd79a90b41285e3e6f27ebccd49f21bff2d1c520
narminaliyeeva <ld8kaxn18@code.edu.az>
2024-05-22 13:45:19 +0400

    commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <section>
+    <div>
+       
+        <div className="navbar">
+          <button>Tasty</button>
+          <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Speciatelist</li>
+            <li>Reservation</li>
+            <li>Blog</li>
+            <li>Account</li>
+            <li>Contact</li>
+          </ul>
+          <button>Admin</button>
+        </div>
+    <div className='text'>
+    <h1>Book a table for yourself at a time convenient for you</h1>
+    </div>
+        
+    </div>
+    </section>
+  )
+}
+
+export default App
